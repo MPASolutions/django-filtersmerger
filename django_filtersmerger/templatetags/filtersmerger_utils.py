adfc6b7a5afbf09f0b72f466eb79aeafa350b0c4
Alessandro Regolini <regolini@mpasol.it>
2021-01-13 09:35:17 +0100

    generalized filter parameter

---
+++ django_filtersmerger/templatetags/filtersmerger_utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django import template
+from django.conf import settings
+import importlib
+
+register = template.Library()
+
+
+@register.filter('request_filter_param')
+def request_filter_param(class_name_searched):
+    for class_path in settings.FILTER_MERGER_CLASSES:
+        module_path, class_name = class_path.rsplit('.', 1)
+        module = importlib.import_module(module_path)
+        filter_class = getattr(module, class_name)
+        if class_name_searched == class_name:
+            return getattr(filter_class, 'PARAM')
+    return None
