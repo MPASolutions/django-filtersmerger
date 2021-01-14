16425b8eb425f8d2b2ee1605f4093e8ff2233bff
perathoner <perathoner@mpasol.it>
2021-01-14 16:49:53 +0100

    template tags per ottenre tutti i params

--- django_filtersmerger/templatetags/filtersmerger_utils.py
+++ django_filtersmerger/templatetags/filtersmerger_utils.py
@@ -16,5 +16,17 @@ def request_filter_param(class_name_searched):
         module = importlib.import_module(module_path)
         filter_class = getattr(module, class_name)
         if class_name_searched == class_name:
-            return getattr(filter_class, 'PARAM')
+            return getattr(filter_class, 'PARAM', None)
     return None
+
+
+@register.simple_tag(takes_context=True)
+def get_request_filter_params(context):
+    filter_params = {}
+    for class_path in settings.FILTER_MERGER_CLASSES:
+        module_path, class_name = class_path.rsplit('.', 1)
+        module = importlib.import_module(module_path)
+        filter_class = getattr(module, class_name)
+        filter_params[class_name] = getattr(filter_class, 'PARAM', None)
+    context['filter_params'] = filter_params
+    return ''
