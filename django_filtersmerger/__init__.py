e3ab0120b5778d34cc78cb0ee392548dde33d149
Alessandro Regolini <alessandro.regolini@enogis.it>
2025-12-17 11:22:50 +0000

    Edit __init__.py

--- django_filtersmerger/__init__.py
+++ django_filtersmerger/__init__.py
@@ -1,6 +1,6 @@
 from django_filtersmerger.filter import RequestFilter  # noqa: F401
 from django_filtersmerger.merger import FilterMerger  # noqa: F401
 
-__version__ = "25.2.1"
+__version__ = "25.12.0"
 
 default_app_config = "django_filtersmerger.apps.DjangoFiltersMergerConfig"
